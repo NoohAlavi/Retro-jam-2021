34784202b4a3653b8aaa54e1af27c86c952a4930
Nooh Alavi <noohalavi@gmail.com>
2021-03-06 10:38:19 -0500

    bug still there ._.

--- Turret/Turret.cs
+++ Turret/Turret.cs
@@ -5,6 +5,8 @@ public class Turret : Node2D
     public override void _Process(float delta)
     {
         var enemies = GetTree().Root.GetNode<Node2D>("World/Path2D");
-        LookAt((enemies.GetChild<Node2D>(enemies.GetChildren().Count - 1).GlobalPosition - GlobalPosition));
+        Enemy target = enemies.GetChild<Enemy>(0);
+        LookAt(target.GlobalPosition);
+        GD.Print("Target: " + target.Name + ", Rotation: " + RotationDegrees);
     }
 }
