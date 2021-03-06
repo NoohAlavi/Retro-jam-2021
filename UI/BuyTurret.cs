3c19b429d248d7315a9a6c1d99d095a719dbc227
Nooh Alavi <noohalavi@gmail.com>
2021-03-06 12:11:24 -0500

    Added new buy turret system

---
+++ UI/BuyTurret.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+public class BuyTurret: Button
+{
+	[Export]
+	public float price;
+	[Export]
+	public string turretName;
+
+	private UI uI;
+
+    public override void _Ready()
+    {
+        Connect("pressed", this, "OnButtonPressed");
+		uI = GetParent().GetParent<UI>();
+    }
+
+    public override void _Process(float delta)
+    {
+        if (uI.money < price)
+		{
+			Disabled = true;
+		} else
+		{
+			Disabled = false;
+		}
+    }
+
+	private void OnButtonPressed()
+	{
+		float money = uI.money;
+		if (money >= price)
+		{
+			money -= price;
+			GD.Print(turretName + " bought for $" + price + ", $" + money + " remaining");
+			uI.money = money;
+		}
+	}
+}
--- UI/UI.cs
+++ UI/UI.cs
@@ -2,7 +2,6 @@ using Godot;
 
 public class UI : Control
 {
-    private Button button1;
     private Label  coinLabel;
 
     [Export]
@@ -10,8 +9,6 @@ public class UI : Control
 
     public override void _Ready()
     {
-        button1 = GetNode<Button>("Buttons/Button_1");
-        button1.Connect("pressed", this, "onButton1Pressed");
 
         coinLabel = GetNode<Label>("CoinsLabel");
     }
@@ -19,19 +16,6 @@ public class UI : Control
     public override void _Process(float delta)
     {
         coinLabel.Text = "Coins: $" + money;
-
-        if (money < 5)
-        {
-            button1.Disabled = true;
-        } else
-        {
-            button1.Disabled = false;
-        }
-    }
-
-    private void onButton1Pressed()
-    {   
-        money -= 5f;
-        GD.Print("Bought Level 1 Turret!");
     }
 }
+
